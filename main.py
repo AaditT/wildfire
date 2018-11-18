a6e7e9436e9d487996d399692841ea95cda03f67
AaditT <aadit.trivedi03@gmail.com>
2018-11-18 11:04:02 -0800

    add precipitation forecast

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pyowm
 import ulmo
 import config
+from weatherbit.api import Api
+api = Api(config.weatherbit_key)
+
 
 # GUIDELINES: https://www.ncei.noaa.gov/news/estimating-wildfire-risk-new-tool
 
@@ -9,8 +12,18 @@ def checkWeather():
     observation = owm.weather_at_place('London,GB')
     w = observation.get_weather()
     return [w.get_wind(), w.get_humidity(), w.get_temperature('celsius')]
+
 '''
 def checkDrought():
     return ulmo.cpc.drought.get_data(state="CA", climate_division=None, start=None, end=None, as_dataframe=False)
 print(checkDrought())
 '''
+
+def getPrecipForecast(city, state):
+    api.set_granularity('daily')
+    forecast = api.get_forecast(city=city, state=state, country="US")
+    precip_forecast = forecast.get_series(['precip'])
+    print(precip_forecast)
+    
+ 
+# getPrecipForecast("San Jose", "CA")
