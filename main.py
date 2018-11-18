19d7faffaed708647fdecc4364dab52f3711c42b
AaditT <aadit.trivedi03@gmail.com>
2018-11-18 10:52:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pyowm
 import ulmo
+import config
 
 # GUIDELINES: https://www.ncei.noaa.gov/news/estimating-wildfire-risk-new-tool
 
 def checkWeather():
-    owm = pyowm.OWM('4ab25daa700e4f5b38ccce7a5c50f9dd')
+    owm = pyowm.OWM(config.owm_key)
     observation = owm.weather_at_place('London,GB')
     w = observation.get_weather()
     return [w.get_wind(), w.get_humidity(), w.get_temperature('celsius')]
