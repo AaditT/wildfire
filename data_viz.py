f3cb7e7d5e41da93c05d8e6e3ccf0ebdae62ea14
AaditT <aadit.trivedi03@gmail.com>
2018-11-19 22:36:20 -0800

    update

--- data_viz.py
+++ data_viz.py
@@ -1,18 +1,24 @@
 import main
 import matplotlib.pyplot as plt
 
-y_vals = []
+fires_by_county = main.experimental()
+
+x_vals = []
 counter = 1
 while counter < 53:
-    y_vals.append(counter)
+    x_vals.append(counter)
     counter += 1
 
-counties_with_fires = []
+odd_num_52 = []
+odd_counter = 0
+while odd_counter < 53:
+    odd_num_52.append(odd_counter)
+    odd_counter += 2
 
 def barGraph():
-    plt.bar(y_vals, main.experimental(), align='center', alpha=0.5, color='r', width=0.8)
-    plt.xticks([1,2,35, 42])
-    plt.ylabel('Experimental Chance of Wildfire')
+    plt.bar(x_vals, main.experimental(), align='center', alpha=0.5, color='r', width=0.8)
+    plt.xticks(odd_num_52)
+    plt.ylabel('Experimental Chance of Wildfire (%)')
     plt.title('California County Code')
     plt.show()
 
