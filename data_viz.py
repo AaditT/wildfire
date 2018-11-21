a6fef171143bb8695655df34e1b7c300872b26b2
AaditT <aadit.trivedi03@gmail.com>
2018-11-20 16:26:26 -0800

    update

--- data_viz.py
+++ data_viz.py
@@ -22,19 +22,22 @@ while even_counter < 55:
     even_num_54.append(even_counter)
     even_counter += 2
 
-# function that creates the bar graph
-def barGraph_2015(csv_in):
-    plt.bar(x_vals, main.experimental(csv_in), align='center', alpha=0.5, color='r', width=0.8)
+# functions that creates the bar graph for each year accordingly
+def barGraph_2015():
+    plt.bar(x_vals, main.experimental(csv_data_2015), align='center', alpha=0.5, color='r', width=0.8)
     plt.xticks(even_num_54)
     plt.ylabel('Experimental Chance of Wildfire (%)')
     plt.title('California County Code (2015)')
     plt.show()
 
-def barGraph_2016(csv_in):
-    plt.bar(x_vals, main.experimental(csv_in), align='center', alpha=0.5, color='r', width=0.8)
+def barGraph_2016():
+    plt.bar(x_vals, main.experimental(csv_data_2016), align='center', alpha=0.5, color='r', width=0.8)
     plt.xticks(even_num_54)
     plt.ylabel('Experimental Chance of Wildfire (%)')
     plt.title('California County Code (2016)')
     plt.show()
 
-barGraph_2015(csv_data_2015)
+# 2015 data:
+# barGraph_2015()
+# 2016 data:
+# barGraph_2016()
